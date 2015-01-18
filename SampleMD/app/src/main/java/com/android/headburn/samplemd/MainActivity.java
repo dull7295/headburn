f5d489f1ce82eba12c7cb7a290997f394fce5e7d
dull7295 <murali@ou.edu>
2015-01-17 19:01:33 -0500

    Changed Some Code in Oncreate() metod

--- SampleMD/app/src/main/java/com/android/headburn/samplemd/MainActivity.java
+++ SampleMD/app/src/main/java/com/android/headburn/samplemd/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add some code here in fact this is my first commit.
     }
 
 
